9822f72e3f8e2b58b8ba4ee60775c831dd48237e
natyfromwonderland <natasha.shilyaeva@gmail.com>
2023-12-14 13:47:30 +0100

    final styling

--- botConfig.py
+++ botConfig.py
@@ -4,7 +4,7 @@
 myBotName = "Lexxy the Space Explorer"
 botTimeZone = "Europe/Paris"  ##See the list full below
 botAvatar = "/static/lexxy.gif" ##This can either be an image in your static folder or a web url to an image
-chatBG = "/static/bg-dark.png"  ##This can either be an image in your static folder or a web url to an image
+chatBG = "/static/bg-planet.png"  ##This can either be an image in your static folder or a web url to an image
 useGoogle = "no" ## yes or no if you want Google links provided when your Bot does not have an answer
 confidenceLevel = 0.75 ##Bot confidence level - must be betwee. 0.00 and 1, default is 0.65
 
